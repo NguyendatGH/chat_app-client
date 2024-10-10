a0bfbe01e40289ca1d41e5c6555af7d0e5dc610a
NguyenDatGH <tandatbtqb@gmail.com>
2024-10-10 21:23:45 +0700

    add custom hooks & errorHandler.ts

---
+++ src/hooks/useOnScreen.tsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+export default function useOnScreen(ref: React.RefObject<HTMLDivElement>) {
+  const [isIntersecting, setIntersecting] = useState(false);
+
+  const observer = new IntersectionObserver(([entry]) =>
+    setIntersecting(entry.isIntersecting)
+  );
+
+  useEffect(() => {
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [ref]);
+
+  return isIntersecting;
+}
---
+++ src/utils/errorHandler.ts
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const errorHandler  = (error : unknown) : string =>{
+    let message = "";
+    if(axios.isAxiosError(error) && error.response){
+        let errorMessage = null;
+        if (error.response.data) {
+            errorMessage = error.response.data.message;
+        }
+        return(message = errorMessage ? errorMessage : "unknown error from backend!");
+    }
+
+    if(error instanceof Error){
+        return (message = error.message);
+    }
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    return (message = "something went wrong!"+ String(error));
+};
+
+export default errorHandler;
